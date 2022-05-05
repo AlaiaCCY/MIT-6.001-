cd5d4726204edd38c06e39e2f263f40ca136a2ab
Alaia <chunyc@uw.edu>
2022-05-05 01:29:20 -0700

    Create L1_What_is_computation.py

---
+++ InClass/L1_What_is_computation.py
@@ -0,0 +1,20 @@
+# Lecture 1 what is computation?
+# In class Notes
+
+# 3+2 does not just peek to programmer
+3+2
+# This print out!
+print(3+2)
+
+# remainder is %
+print(3%1)
+# power is **
+print(3**2)
+
+# = is assignment of a value to variable which is variable = value
+# unlike java, do not need to declare the type of variable first
+pi = 3.1413
+print('pi is', pi)
+# re-bind value does not destroy the old value, it store in the memory somehow
+pi = pi +1
+print('new pi value is', pi)
