e82fca3bd42ac8de2c42ea36810ebea55490c006
Alaia <chunyc@uw.edu>
2022-05-08 02:44:30 -0700

    PS0 finished

---
+++ HW/ps0/PS0.py
@@ -0,0 +1,11 @@
+# =============================================================================
+# MIT 6.001 PS0
+# Chunyu Chen(Alaia)
+# =============================================================================
+import numpy
+import pylab
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("X**y =", x**y)
+print("log(x) =", numpy.log2(x))
